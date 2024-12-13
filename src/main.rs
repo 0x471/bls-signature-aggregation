327fa0e64a82b4b2c123515d344dba204d11ed18
0x471 <0x471@protonmail.com>
2024-12-13 22:11:42 +0000

    implement key pair generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use ark_bls12_381::{Fr, G2Projective};
+use ark_ec::{CurveGroup, PrimeGroup};
+use ark_ff::{PrimeField, UniformRand};
+use ark_std::rand::{rngs::StdRng, SeedableRng};
+use std::ops::Mul;
+
+fn generate_keys(rng: &mut StdRng) -> (Fr, G2Projective) {
+    let sk = Fr::rand(rng);
+    let pk = G2Projective::generator().mul(sk);
+    (sk, pk)
+}
+
 fn main() {
-    println!("Hello, world!");
+    // this is not suitable for cryptography :)
+    let mut rng = StdRng::seed_from_u64(42);
+
+    let (sk1, pk1) = generate_keys(&mut rng);
+    let (sk2, pk2) = generate_keys(&mut rng);
+    let (sk3, pk3) = generate_keys(&mut rng);
+
+    println!("Secret Keys: {}, {}, {}", sk1, sk2, sk3);
+    println!("Public Keys: {:?}, {:?}, {:?}", pk1, pk2, pk3);
 }
