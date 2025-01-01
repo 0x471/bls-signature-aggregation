98eed9b0929df2d0799440ce80d4b55c9045dc08
0x471 <0x471@protonmail.com>
2025-01-01 19:23:14 +0000

    use secure rng for key generation

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use ark_ec::{
 };
 use ark_ff::{PrimeField, UniformRand};
 use ark_std::{
-    rand::{rngs::StdRng, SeedableRng},
+    rand::{rngs::OsRng, SeedableRng},
     Zero,
 };
 use std::ops::Mul;
 
-fn generate_keys(rng: &mut StdRng) -> (Fr, G2Projective) {
+fn generate_keys(rng: &mut impl rand::RngCore) -> (Fr, G2Projective) {
     let sk = Fr::rand(rng);
     let pk = G2Projective::generator().mul(sk);
     (sk, pk)
@@ -63,8 +63,7 @@ fn verify_aggregated(
     signature_pairing == pairing_result
 }
 fn main() {
-    // this is not suitable for cryptography :)
-    let mut rng = StdRng::seed_from_u64(42);
+    let mut rng = OsRng;
 
     let (sk1, pk1) = generate_keys(&mut rng);
     let (sk2, pk2) = generate_keys(&mut rng);
